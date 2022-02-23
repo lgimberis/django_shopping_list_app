a388c3e99c4940844b3737bb236ac45f8bf85805
lgimberis <lukasgimberis@gmail.com>
2022-02-23 13:51:24 +0000

    Minor readability improvements to the 'strikethrough-toggle' script

--- static/js/strikethrough_toggle.js
+++ static/js/strikethrough_toggle.js
@@ -2,20 +2,22 @@
 function manageStrikethrough(button){
     hidden = document.getElementById(button.id.replace("button", "hidden"))
     let initial_strikethrough = button.style.getPropertyValue("text-decoration") == "line-through";
-    let strikethrough = !initial_strikethrough
-    if(!strikethrough) {
+    if(initial_strikethrough) {
+        // Remove strikethrough
+        button.style.setProperty("text-decoration", "");
+
         button.classList.remove("btn-secondary");
         button.classList.add("btn-primary");
         button.classList.remove("item-remove");
         hidden.value = "keep";
-
-        button.style.setProperty("text-decoration", "");
     } else {
+        //Add strikethrough
+        button.style.setProperty("text-decoration", "line-through");
+
         button.classList.add("btn-secondary");
         button.classList.remove("btn-primary");
         button.classList.add("item-remove");
-        button.style.setProperty("text-decoration", "line-through");
         hidden.value = "delete";
     }
-    return strikethrough;
+    return !initial_strikethrough;
 }
