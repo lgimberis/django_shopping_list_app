f033fd2572468a1443466572f0b51459e02039b3
Lukas Gimberis <lukasgimberis@gmail.com>
2022-09-13 13:48:42 +0100

    Fix signal to work for new groups with no categories.

--- signals.py
+++ signals.py
@@ -4,5 +4,8 @@ from .models import Category
 
 def assign_order(sender, instance, created, **kwargs):
     if created:
-        instance.sorting_weight = Category.objects.filter(group=instance.group).order_by('-sorting_weight')[0].sorting_weight + 1
-        instance.save()
+        group_categories = Category.objects.filter(group=instance_group)
+        if group_categories.count() > 1:
+            # Only set this if another category exists. No 'else' block needed - default value 0 is correct.
+            instance.sorting_weight = group_categories.order_by('-sorting_weight')[0].sorting_weight + 1
+            instance.save()
